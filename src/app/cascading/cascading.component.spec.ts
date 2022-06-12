ec14df5081811c7f7a556aec02477607a45d16ba
dat <hoquocdat1405@gmail.com>
2022-06-12 19:35:53 +0700

    them cascading

---
+++ src/app/cascading/cascading.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CascadingComponent } from './cascading.component';
+
+describe('CascadingComponent', () => {
+  let component: CascadingComponent;
+  let fixture: ComponentFixture<CascadingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CascadingComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CascadingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/cascading/cascading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cascading',
+  templateUrl: './cascading.component.html',
+  styleUrls: ['./cascading.component.scss']
+})
+export class CascadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
